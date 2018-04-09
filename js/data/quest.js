f011bf452084098f1b8aa7f7d646c58275368dda
Evgenii Shchepotev <evgenii.schepotiev@gmail.com>
2018-04-09 15:54:11 +0300

    add quest data

--- js/data/quest.js
+++ js/data/quest.js
@@ -17,3 +17,18 @@ export const changeLevel = (game, level) => {
     level
   });
 };
+
+export const canContinue = (game) => game.lives - 1 > 0;
+
+export const die = (game) => {
+  if (!canContinue(game)) {
+    throw new Error(`You can't continue anymore`);
+  }
+
+  const lives = game.lives - 1;
+
+  return Object.assign({}, game, {
+    lives
+  });
+};
+
